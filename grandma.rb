8ee680829f454d7413bedcbabecf29d8f548ec5f
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-08-31 03:53:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma
   if ("I LOVE YOU GRANDMA!")
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif ("Hi, Nana, how are you?" || "Hi!")
-    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   elsif ("WHAT DID YOU EAT TODAY?" || "WHAT?")
-    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   end
 end
