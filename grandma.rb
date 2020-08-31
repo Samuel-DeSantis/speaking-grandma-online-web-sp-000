c68684654bcd76ff9aa93b547adbaba61f724790
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-08-31 03:55:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(input)
   if (input === "I LOVE YOU GRANDMA!")
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif (input === "Hi, Nana, how are you?" || input === "Hi!")
+  elsif (input === "Hi Nana, how are you?" || input === "Hi!")
     return "HUH?! SPEAK UP, SONNY!"
   elsif (input === "WHAT DID YOU EAT TODAY?" || input === "WHAT?")
     return "NO, NOT SINCE 1938!"
